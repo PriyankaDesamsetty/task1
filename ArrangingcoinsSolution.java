1a67d85b4782c3dbd25f1ce5d690a18fa2317b50
PriyankaDesamsetty <desamsettypriyanka@gmail.com>
2019-03-11 18:15:27 +0530

    input

--- ArrangingcoinsSolution.java
+++ ArrangingcoinsSolution.java
@@ -1,4 +1,28 @@
 /*Arranging Coins
+input
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+
 */
 class ArrangingcoinsSolution {
     public int arrangeCoins(int n) {
